451c6bf056e34ff03deb1dfc88ff942c88cfdf04
jhon <ortizz@gmail.com>
2024-04-03 16:57:25 -0500

    porfolio

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+
+function App() {
+
+  return (
+    <>
+    <main className="font-roboto flex justify-center items-center w-screen h-screen">
+      <h1 className="text-3xl font-bold text-blue-500">Hello world</h1>
+      
+      
+    </main>
+    <body>
+
+
+      <div class="w-64 h-64 bg-blue-400 border border-blue-400 rounded-md shadow-md p-4">
+  
+      </div>
+
+    </body>
+    </>
+  )
+}
+
+export default App
